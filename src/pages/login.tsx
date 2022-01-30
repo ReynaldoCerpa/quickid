50b0e49f67c9a70de46e1c4a120aed06f0a5052f
Reynaldo <reynaldocerpa07@gmail.com>
2022-01-29 22:31:55 -0800

    new colors

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 //import styles from "../styles/Login.module.css"
 const login = () => {
     return (
-        <section className="flex justify-center pt-[6rem] h-screen bg-[#2e9cca]">
-            <Box className="flex justify-center rounded-[9px] pt-[2rem] w-[25rem] drop-shadow-xl h-[30rem] bg-white p-[2rem]">
+        <section className="flex justify-center pt-[9rem] h-screen ">
+            <Box className="flex justify-center rounded-[9px] pt-[2rem] w-[25rem] drop-shadow-lg h-[30rem] bg-white p-[2rem]">
                 <Box className="flex flex-col relative w-[20rem]">
-                    <Text className="text-center test-class font-raleway text-[2.5rem]">Quickid</Text>
-                    <Text className="text-center font-source-sans text-[1.5rem] mt-[.5rem]" fontWeight="semibold">Iniciar sesión</Text>
-                    <Input className="Button" placeholder="Usuario" />
-                    <Input className="Button" placeholder="Contraseña" type="password" />
+                    {/* <Text className="text-center test-class font-raleway text-[2.5rem]">Quickid</Text> */}
+                    <Text className="text-center font-source-sans text-[2rem] mt-[.5rem] mb-[2rem]" fontWeight="semibold">Iniciar sesión</Text>
+                    <Input className="Input" placeholder="Usuario" />
+                    <Input className="Input" placeholder="Contraseña" type="password" />
                     <Link href="https://youtube.com">
                         <Text color="blue.500" fontWeight="bold" className="mt-1 text-[.9rem]">¿Olvidó su contraseña?</Text>
                     </Link>
@@ -19,7 +19,11 @@ const login = () => {
                         <Link href="https://youtube.com">
                             <Text color="blue.500" fontWeight="bold" className="">Registrarse</Text>
                         </Link>
-                        <Button>Iniciar sesión</Button>
+                        <Button 
+                        bg="#25274d"
+                        color="white"
+                        _hover={{bg: "#353878"}}
+                        >Iniciar sesión</Button>
                     </Box>
                 </Box>
             </Box>
