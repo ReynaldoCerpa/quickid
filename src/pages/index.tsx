dbe9404ae0a9aba4feb365a95a092221530a24e1
Reynaldo <reynaldocerpa07@gmail.com>
2022-01-30 17:52:58 -0800

    experimenting styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,16 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div >
-      
+    <div className="w-screen h-screen flex bg-primary ">
+      <h2
+        className="
+      m-auto text-white 
+      font-raleway text-[6rem]
+      whitespace-pre-line
+      ">Siempre a
+      tiempo.</h2>
 
-      
+
     </div>
   )
 }
